6854fa21711a3c535571a9d3e207d0779da0e069
Joao Victor <joaovictor.programacao@simplesvarejo.com.br>
2025-12-12 21:54:18 -0300

    Criação dos endpoints de autenticação: login, refresh, register e me

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserMeSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserMeSerializer(request.user)
+        return Response(serializer.data)
